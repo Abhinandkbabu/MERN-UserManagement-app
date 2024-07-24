670e42ac9b5b0dc3b591cbdefc7a58adef4d6c0f
Abhinand Babu K <abhinandkbabu@gmail.com>
2024-07-24 11:00:46 +0530

    data base connected and user model created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-app.listen(3000, ()=> console.log("Server listning in 3000"))
+mongoose
+.connect(process.env.MONGO)
+.then(()=>console.log('database connected'))
+.catch(err=>console.log(err))
+
+app.listen(process.env.PORT, ()=> console.log("Server listning in 3000"))
---
+++ api/model/userModel.js
@@ -0,0 +1,27 @@
+const { Timestamp } = require('bson');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required: true,
+        unique: true
+    },
+
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+        type:String,
+        required: true,
+    },
+
+
+},{Timestamp :true})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
