02d516a65e2f8b9aa282c4c4f21a1eececa9a27b
Abhinand Babu K <abhinandkbabu@gmail.com>
2024-07-28 21:57:13 +0530

    create sign in api route

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,5 +1,7 @@
 import User from '../model/userModel.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next)=>{
     try{
@@ -10,5 +12,21 @@ export const signup = async (req,res,next)=>{
     } catch (error) {
         console.log(error)
         next(error); 
+    }
+};
+
+export const signin = async (req, res, next) => {
+    try{
+        const {email,  password} = req.body
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(404, 'Invalid Email Address or Password'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) next(errorHandler(401, 'Invalid Email Address or Password'))
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        const { password: _, ...user } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 7200000) //two hours
+        res.cookie('access_token', token, {httpOnly : true, expires: expiryDate}).status(200).json(user)
+    }catch (err){
+        next(err) 
     }
 }
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/authController.js';
+import { signin, signup } from '../controller/authController.js';
 
 const router = express.Router();
 
 router.route('/signup')
 .post(signup)
+
+router.route('/signin')
+.post(signin)
 
 export default router
