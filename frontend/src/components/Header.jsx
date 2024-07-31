4380b9c9a225b42afb649e36bb0d85bda30b9dff
Abhinand Babu K <abhinandkbabu@gmail.com>
2024-07-31 11:47:01 +0530

    Header Updated and private Route Created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+
+  const { currentUser } = useSelector((state) => state.user)
+
   return (
     <div className='bg-slate-300'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -11,7 +15,9 @@ const Header = () => {
         <ul className='flex gap-4'>
             <Link to='/'><li>Home</li></Link>
             <Link to='/about'><li>About</li></Link>
-            <Link to='/sign-in'><li>Sign In</li></Link>
+            <Link to='/profile'>
+            {currentUser ? (<img src={currentUser.profilePicture} alt='User' className='h-7 w-7 rounded-full object-cover' />) : (<li>Sign In</li>)}
+            </Link>
         </ul>
       </div>
     </div>
